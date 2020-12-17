1b6846b882440d2de4bbc849d410af89044ed610
Rodrigo Sommacal <rodrigosommacal@gmail.com>
2020-12-17 14:06:50 +0100

    User authentication created

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :sessions, only: %i[create]
+  resources :registrations, only: %i[create]
+  delete :logout, to: "sessions#logout"
+  get :logged_in, to: "sessions#logged_in"
+
+  root to: "pages#home"
 end
